3f75ec4387c89fd4cd989ce331e42fe1e36f5c27
Ankush Kumar <ankushkumar@Ankushs-MacBook-Air.local>
2026-04-21 23:17:05 +0530

    fixes

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js on EC2 🚀\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

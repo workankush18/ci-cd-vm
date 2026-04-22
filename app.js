80fd201903d1563084a615ae0adb7818850fb882
Ankush Kumar <ankushkumar@Ankushs-MacBook-Air.local>
2026-04-23 01:25:25 +0530

    fixes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node.js on EC2 33🚀\n");
+  res.end("Hello from Node.js on EC2 ---------33🚀\n");
 });
 
 server.listen(PORT, () => {
